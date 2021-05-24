7f77855787c6fff0639beaaece2f86f9fe52eeb7
Alen Makaric <makaric92@gmail.com>
2021-05-24 02:22:42 +0200

    using one state instead of multiple states

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,23 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-  const clickHandler = () => {
-    console.log('clicked!!');
-  };
+  const [title, setTitle] = useState(props.title);
+  // let title = props.title;
 
-  // clicked() {
-  //   console.log('clicked');
-  // }
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log('clicked!!', title);
+  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
